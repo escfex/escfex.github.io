4ea84ecd395a05d9ac32c620dc837691da49b8e5
escfex <escfex@gmail.com>
2024-01-25 10:04:01 +0800

    feat: workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,30 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-	site: 'https://escfex.github.io',
-	base: '/',
+  integrations: [starlight({
+    title: 'EscFeX',
+    social: {
+      github: 'https://github.com/escfex'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), react()],
+  site: 'https://escfex.github.io',
+  base: '/'
 });
